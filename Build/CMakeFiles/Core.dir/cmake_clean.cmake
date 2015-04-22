b364994970c879a7e94cce154ae95df4b4771b8a
Justus Flerlage <justusflerlage@taista.net>
2015-04-21 19:06:19 -0700

    changed ignore rules

---
+++ Build/CMakeFiles/Core.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Core.dir/home/justus/Dokumente/college/cs40a/project01_team01/Source/PhatChat/Core/Packet.cpp.o"
+  "libCore.pdb"
+  "libCore.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Core.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Source/PhatChat/Client/Application.cpp
+++ Source/PhatChat/Client/Application.cpp
@@ -1,6 +1,17 @@
 #include <PhatChat/Client/Application.hpp>
+#include <SFML/Network.hpp>
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
 
 int PhatChat::Client::Application::main ( const std::vector <std::string> & arguments )
 {
-	return 0 ;
+	Fl_Window * window = new Fl_Window ( 340 , 180 ) ;
+	
+	Fl_Box * box = new Fl_Box ( 20 , 40 , 300 , 100 , "Hello World" ) ;
+		
+	window->end ( ) ;
+	window->show ( ) ;
+
+	return Fl::run ( ) ;
 }
