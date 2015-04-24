5e061b7eada0f6c122c334592575b0fc1ffdb9f0
Justus Flerlage <justusflerlage@taista.net>
2015-04-23 22:13:31 -0700

    extended Application class

--- Source/PhatChat/Server/Application.cpp
+++ Source/PhatChat/Server/Application.cpp
@@ -1,6 +1,61 @@
 #include <PhatChat/Server/Application.hpp>
+#include <PhatChat/Core/DefaultPort.hpp>
+#include <SFML/Network.hpp>
+#include <iostream>
+
+PhatChat::Server::Application::Application ( ) :
+    clientManager ( * this )
+{
+}
+
+sf::TcpListener & PhatChat::Server::Application::getSocket ( )
+{
+    return this->socket ;
+}
+const sf::TcpListener & PhatChat::Server::Application::getSocket ( ) const
+{
+    return this->socket ;
+}
+
+PhatChat::Server::ClientManager & PhatChat::Server::Application::getClientManager ( )
+{
+    return this->clientManager ;
+}
+const PhatChat::Server::ClientManager & PhatChat::Server::Application::getClientManager ( ) const
+{
+    return this->clientManager ;
+}
+
+void PhatChat::Server::Application::setRunning ( bool running )
+{
+    this->running = running ;
+}
+bool PhatChat::Server::Application::isRunning ( ) const
+{
+    return this->running ;
+}
 
 int PhatChat::Server::Application::main ( const std::vector <std::string> & arguments )
 {
+    // fetch port from command line parameters, use 20900 as default and start listening for
+    // data on the socket
+    const unsigned short port = arguments.size ( ) > 1 ? std::atoi ( arguments [ 1 ].c_str ( ) ) : PhatChat::defaultPort ;
+    if ( this->socket.listen ( port ) != sf::Socket::Done )
+    {
+        std::cout << port << " is already in use!\n" ;
+        return -1 ;
+    }
+    std::cout << "listening on port " << port << "\n" ;
+
+    while ( this->isRunning ( ) )
+    {
+        this->clientManager.update ( ) ;
+    }
+
 	return 0 ;
+}
+
+void PhatChat::Server::Application::stop ( )
+{
+    this->setRunning ( false ) ;
 }
--- Source/PhatChat/Server/Application.hpp
+++ Source/PhatChat/Server/Application.hpp
@@ -1,15 +1,47 @@
 #pragma once
 
 #include <PhatChat/Core/Application.hpp>
+#include <PhatChat/Server/ClientManager.hpp>
+#include <SFML/Network.hpp>
 
 namespace PhatChat
 {
 	namespace Server
 	{
+        // class for the server application
 		class Application : public PhatChat::Application
 		{
 			public :
+                // default constructor
+                Application ( ) ;
+
+                // accessor for socket
+                sf::TcpListener & getSocket ( ) ;
+                // const accessor for socket
+                const sf::TcpListener & getSocket ( ) const ;
+
+                // accessor for clientManager
+                PhatChat::Server::ClientManager & getClientManager ( ) ;
+                // const accessor for clientManager
+                const PhatChat::Server::ClientManager & getClientManager ( ) const ;
+
+                // mutator for running
+                void setRunning ( bool running ) ;
+                // accessor for running
+                bool isRunning ( ) const ;
+
+                // main function
 				int main ( const std::vector <std::string> & arguments ) ;
+
+                // stops the server, setting running to false
+				void stop ( ) ;
+
+            private :
+                bool running = true ;
+
+                sf::TcpListener socket ;
+
+                PhatChat::Server::ClientManager clientManager ;
 		} ;
 	}
 }
