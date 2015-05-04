9c939c4dcc29455cc9de9ff6d7ddabb2603c064c
Justus Flerlage <justusflerlage@taista.net>
2015-05-04 16:17:09 -0700

    merge commit

--- Source/PhatChat/Client/Application.cpp
+++ Source/PhatChat/Client/Application.cpp
@@ -6,6 +6,24 @@
 #include <PhatChat/Core/PingPacket.hpp>
 #include <iostream>
 
+
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
+#include <FL/fl_ask.H>
+#include <FL/Fl_Input.H>
+#include <FL/Fl_Gl_Window.H>
+#include <FL/Fl_Double_Window.H>
+#include <FL/Fl_Text_Display.H>
+#include <FL/Fl_Menu.H>
+#include <FL/Fl_Output.H>
+#include <FL/Fl_Text_Editor.H>
+#include <FL/Fl_Multiline_Input.H>
+#include <FL/Fl_Multiline_Output.H>
+#include <FL/Fl_Pack.H>
+#include <FL/Fl_Tile.H>
+#include <FL/Fl_Return_Button.H>
+
 PhatChat::Client::Application::Application ( ) :
 	receiveThreadHandle ( & PhatChat::Client::Application::receiveThreadFunction , this )
 {
@@ -15,6 +33,24 @@ int PhatChat::Client::Application::main ( const std::vector <std::string> & argu
 {
 	// change FLTK design
 	Fl::scheme ( "gtk+" ) ;
+
+Fl_Window	*window = new Fl_Window(0,0, 800, 800);
+    Fl_Tile		*phatTile = new Fl_Tile(0, 0, 800, 800);
+    Fl_Input	*phatInput = new Fl_Input(0,400,600,300);
+    Fl_Output	*phatChatOutput = new Fl_Output( 0, 0, 600, 400, "Conversation:");
+    Fl_Output 	*phatNameOutput = new Fl_Output ( 600, 0, 200, 800, "Chatters:");
+    Fl_Return_Button *phatReturn = new Fl_Return_Button( 0, 700, 600, 100, "Enter");
+
+    phatInput->color(FL_CYAN);
+    //phatChatOutput->color(FL_BLUE);
+
+    //phatInput->align(FL_ALIGN_INSIDE);
+    phatChatOutput->align(FL_ALIGN_INSIDE);
+    phatChatOutput->align(FL_NORMAL_LABEL);
+
+    window->end();
+    window->show();
+    return Fl::run ( ) ;
 
 	// fetch hostname and port from command line arguments only if all command line arguments were given, use 20900 as default port and
 	// 127.0.0.1 as default hostname and connect to the server
@@ -58,7 +94,15 @@ int PhatChat::Client::Application::main ( const std::vector <std::string> & argu
             // launch receive thread
             this->receiveThreadHandle.launch ( ) ;
 
-            while ( Fl::wait ( ) ) ;
+            while ( Fl::wait ( ) )
+            {
+            	if ( this->chatWindow.isSendPushed ( ) )
+            	{
+            		std::string message = this->chatWindow.getMessage ( ) ;
+            		
+            		this->socket.send ( PhatChat::MessagePacket ( message ).encode ( ) ) ;
+            	}
+            }
 
             std::cout << "Connection to " << this->socket.getRemoteAddress ( ) << ":" << this->socket.getRemotePort ( ) << " closed!\n" ;
         }
@@ -98,6 +142,12 @@ void PhatChat::Client::Application::handlePacket ( sf::Packet packet )
 
 		if ( this->socket.send ( newPacket ) != sf::Socket::Done )
 			this->setRunning ( false ) ;
+	}
+	else if ( operationCode == PhatChat::OperationCode::MESSAGE )
+	{
+		PhatChat::MessagePacket messagePacket ( PhatChat::PingPacket::decode ( packet ) ) ;
+
+		this>chatWindow.addMessage ( messagePacket.getMessage ( ) ) ;
 	}
 	else
 		std::cout << "Operation code is unknown! Skip " << packet.getDataSize ( ) << " bytes." << std::endl ;
